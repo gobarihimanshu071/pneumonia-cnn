236b8b00471e708e914097fa44865e3e33cd8bee
gobarihimanshu071 <himanshugobari000@gmail.com>
2026-04-20 20:50:02 +0530

    Baseline CNN model for pneumonia detection

---
+++ main.py
@@ -0,0 +1,73 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import  matplotlib.pyplot as plt
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    zoom_range=0.2,
+    shear_range=0.2,
+    horizontal_flip=True
+)
+
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_datagen.flow_from_directory(
+    'dataset/train',
+    target_size=(224,224),
+    batch_size=32,
+    class_mode='binary',
+    color_mode='grayscale'   
+)
+
+val_data = val_datagen.flow_from_directory(
+    'dataset/val',
+    target_size=(224,224),
+    batch_size=32,
+    class_mode='binary',
+    color_mode='grayscale'
+)
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+model = Sequential()
+
+model.add(Conv2D(32, (3,3), activation='relu' , input_shape=(224,224,1)))
+model.add(MaxPooling2D(2,2))
+
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(MaxPooling2D(2,2))
+
+model.add(Conv2D(128, (3,3), activation='relu'))
+model.add(MaxPooling2D(2,2))
+
+model.add(Dropout(0.3))
+
+model.add(Flatten())
+
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(1,activation='sigmoid'))
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+history = model.fit(
+    train_data,
+    epochs=10,
+    validation_data=val_data
+)
+
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(['Train','Validation'])
+plt.show()
+
